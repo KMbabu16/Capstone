fb0f606d308017e14086627eab93e32830ffc338
KMbabu16 <mohanbabu.katikapalle@gmail.com>
2025-02-22 09:09:22 +0530

    Add files via upload

--- settings.py
+++ settings.py
@@ -1,9 +1,4 @@
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
-
-DB_NAME = os.environ.get("DB_NAME")
-DB_USER = os.environ.get("DB_USER")
-DB_PASSWORD = os.environ.get("DB_PASSWORD")
-DB_HOST=os.environ.get("DB_HOST")
+DB_NAME='fsnd' 
+DB_USER='postgres' 
+DB_PASSWORD='mohan'
+DB_HOST="localhost:5432"
